0ff4e65bddcf73c239b804947e7b09211e6558c6
Ravshanjon <r.abdurahimov@uic.group>
2025-04-16 23:09:18 +0500

    change docker and playlist

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+FROM python:3.11-alpine
+
+RUN mkdir /app
+
+# Set work directory
+WORKDIR /app
+
+# Environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV CRYPTOGRAPHY_DONT_BUILD_RUST=1
+
+# Install required system dependencies
+RUN apk --no-cache add \
+    gcc \
+    musl-dev \
+    linux-headers \
+    python3-dev \
+    libffi-dev \
+    postgresql-dev \
+    icu-dev \
+    gettext \
+    libpq-dev \
+    glib-dev \
+    poppler-glib \
+    vips-dev \
+    vips-tools \
+    poppler-utils \
+    ffmpeg
+
+# Copy requirements file
+COPY requirements.txt .
+
+# Install Python dependencies
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+# Copy the rest of the project
+COPY . .
+
+# Expose the port the application runs on
+EXPOSE 8000
--- organizations/views.py
+++ organizations/views.py
@@ -91,21 +91,22 @@ class PlaylistsDetailAPIView(APIView):
                 'duration': media.duration
             })
 
-        # playlists = []
-        # for playlist in Playlist.objects.filter(devices=device):
-        #     playlists.append({
-        #         'id': playlist.playlist_id,
-        #         'name': playlist.name,
-        #         'start_time': timezone.localtime(playlist.start_time).strftime('%Y-%m-%d %H:%M:%S'),
-        #         'end_time': timezone.localtime(playlist.end_time).strftime('%Y-%m-%d %H:%M:%S'),
-        #         'medias': media_list
-        #     })
+        playlists = []
+        for playlist in Playlist.objects.filter(devices=device).order_by("start_time"):
+            playlists.append({
+                'id': playlist.playlist_id,
+                'name': playlist.name,
+                'start_time': timezone.localtime(playlist.start_time).strftime('%Y-%m-%d %H:%M:%S'),
+                'end_time': timezone.localtime(playlist.end_time).strftime('%Y-%m-%d %H:%M:%S'),
+                'medias': media_list
+            })
 
         response = {
-            'start_time': timezone.localtime(playlist.start_time).strftime('%Y-%m-%d %H:%M:%S'),
-            'end_time': timezone.localtime(playlist.end_time).strftime('%Y-%m-%d %H:%M:%S'),
-            'exit_password': device.exit_password,
-            'playlist': media_list
+            "server_time": timezone.localtime(timezone.now()).strftime('%Y-%m-%d %H:%M:%S'),
+            "exit_password": device.exit_password,
+            "id": device.device_id,
+            "name": device.name,
+            "playlists": playlists,
         }
 
         return Response(response, status=200)
