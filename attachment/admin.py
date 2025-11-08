6fa56b24bedcf69f925eb183a3addc272e02b66e
xan <abdujalil@MacBook-Air--xan.local>
2025-11-08 18:10:29 +0500

    add attachment app

--- attachment/admin.py
+++ attachment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from attachment.models import Attachment
+
+
 # Register your models here.
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file')
