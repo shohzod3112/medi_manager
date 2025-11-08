43764bfe5b961f85ac2b86ee90e840042c66ad8f
xan <abdujalil@MacBook-Air--xan.local>
2025-11-08 18:02:35 +0500

    add attachment app

--- core/urls.py
+++ core/urls.py
@@ -58,8 +58,8 @@ urlpatterns = [
     path("admin/get-org-db-name/", user_views.get_org_db_name, name="get_org_db_name"),
     path("admin/", admin.site.urls),
     path("health/", health_check, name="health_check"),
-    path("api/", include("apps.urls")),  # Authentication APIs
     path("api/attachments/", include("attachment.urls")),
+    path("api/", include("apps.urls")),  # Authentication APIs
 ]
 
 if settings.DEBUG:
