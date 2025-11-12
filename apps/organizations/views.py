de0b6c8ef7fd42e533129095f3e90455a406aaa8
xan <abdujalil@MacBook-Air--xan.local>
2025-11-12 23:02:17 +0500

    add pagination to files

--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -467,9 +467,3 @@ class FileDetailView(generics.RetrieveUpdateDestroyAPIView):
             organization=profile.organization,
             owner=user,
         )
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        instance.files.all().delete()
-        return Response({"detail": "O‘chirildi"}, status=status.HTTP_200_OK)
