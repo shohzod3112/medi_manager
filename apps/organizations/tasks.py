f45796e8cc1dab77aedd4c0004b6aebf195871b6
xan <abdujalil@MacBook-Air--xan.local>
2025-11-15 00:27:41 +0500

    changes

--- apps/organizations/tasks.py
+++ apps/organizations/tasks.py
@@ -2,20 +2,22 @@ from celery import shared_task
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
+from apps.organizations.models import Organization
+
 User = get_user_model()
 
 
-@shared_task
-def disable_expired_staff():
-    """
-    Disable expired staff users.
-    """
-    now = timezone.now()
-    expired_users = User.objects.filter(
-        is_staff=True,
-        expiration_date__isnull=False,
-        expiration_date__lt=now,
-    )
-
-    if expired_users.exists():
-        expired_users.update(is_staff=False)
+# @shared_task
+# def disable_expired_staff():
+#     """
+#     Disable expired staff users.
+#     """
+#     now = timezone.now()
+#     organizations = Organization.objects.filter(
+#         is_staff=True,
+#         expiration_date__isnull=False,
+#         expiration_date__lt=now,
+#     )
+#
+#     if organizations.exists():
+#         organizations.update(is_active=False)
