9bad0d936ce7574fcccd769ca99eebeef4d15d35
shohzod3112 <93348219+shohzod3112@users.noreply.github.com>
2025-12-10 22:14:44 +0500

    Modify CORS settings for production environment
    Updated CORS_ALLOWED_ORIGINS to include production URLs and commented out local development URLs.

--- core/settings.py
+++ core/settings.py
@@ -118,11 +118,15 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "http://localhost:5173",
-    "https://localhost:5173",
-    "http://127.0.0.1:3000",
+    "https://media-manager.uz",
+    "http://media-manager.uz",
+    "https://www.media-manager.uz",
+    "https://10.170.100.11:8000", 
+    #"http://localhost:3000",
+    #"https://localhost:3000",
+    #"http://localhost:5173",
+    #"https://localhost:5173",
+    #"http://127.0.0.1:3000",
 ]
 
 SIMPLE_JWT = {
