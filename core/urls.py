f8c2a96edf5f3095a1cbb7a2000855a68bb5e00b
xan <abdujalil@MacBook-Air--xan.local>
2025-12-29 21:54:10 +0500

    Dfdf

--- core/urls.py
+++ core/urls.py
@@ -64,8 +64,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# 🔴 ENG OXIRIDA frontend
 urlpatterns += [
     re_path(r"^.*$", frontend_view),
 ]
