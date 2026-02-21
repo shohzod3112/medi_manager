08f7ea785bb3613a8fd21c1043fef3423d11f4a4
xan <abdujalil@MacBook-Air--xan.local>
2026-02-22 00:29:46 +0500

    add  install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+HWID=$(cat /etc/machine-id | sha256sum | awk '{print $1}')
+echo "Server HWID: $HWID"
+
+# Licence kutish
+while [ ! -f ./backend/licence.json ]; do
+    echo "Waiting for licence.json..."
+    sleep 2
+done
+
+echo "Licence found, starting docker..."
+docker compose -f backend/docker-compose.yml up -d
+
+# Wait for web HEALTHY
+until [ "$(docker inspect --format='{{.State.Health.Status}}' media-manager-web)" == "healthy" ]; do
+    echo "Waiting for web container..."
+    sleep 2
+done
+
+echo "Web container is healthy!"
+
+# Source code backup
+tar -czf source_backup.tar.gz backend/
+rm -rf backend/*
+echo "Source archived and deleted!"
