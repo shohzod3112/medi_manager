2e3602a3788e41d21b9cc8d00c1c981f8f90350b
xan <abdujalil@MacBook-Air--xan.local>
2026-02-27 00:00:26 +0500

    Dfdf

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ mkdir -p "$LICENCE_DIR"
 chmod 755 "$LICENCE_DIR"
 
 log "Generating HWID"
-HWID=$(python3 "$BACKEND_DIR/hwid.py")
+HWID=$(cat /etc/machine-id | sha256sum | awk '{print $1}')
 echo "HWID: $HWID"
 
 echo "👉 Generate licence.json on DEV machine"
