54b47c88918ca2f8252e64bcb99be8e4831c5216
xan <abdujalil@MacBook-Air--xan.local>
2026-03-27 10:26:29 +0500

    dfgdg

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,6 @@ set -e
 # ========================
 mkdir -p /app/static /app/staticfiles /app/media /app/logs /tmp
 
-# Ruxsatlarni tuzatish
-chown -R app:app /app/static /app/staticfiles /app/media /app/logs
-chmod -R 755 /app/static /app/staticfiles /app/media /app/logs
-
-
 # Simple logging
 log() { echo "[$(date +'%H:%M:%S')] $1"; }
 error() { echo "ERROR: $1" >&2; exit 1; }
